313661faeacb9036cd2a3ed2a19f957770d40a59
rahidkhan <rahidkhanbannu1@email.com>
2025-09-07 01:16:33 +0430

    add functionality to the sign up router

--- server/Controller/auth.controller.js
+++ server/Controller/auth.controller.js
@@ -1,7 +1,29 @@
-
+import {hashSync} from 'bcrypt'
+import USER_MODEL from '../model/user.model.js';
+import errorHandler from '../utils/errorHandler.js';
 // ----------------------------------------------- sign up function ----------------------------------------------
 
  export async function signUp (req , res ,next ) {
+       const userBody = req.body;
+       userBody.password = hashSync(userBody.password , 10 );
+     try {
+          const userExiste = await USER_MODEL.findOne({email:userBody.email});
           
-          res.send("hellow world ")
+          if(userExiste){
+               return res.status(409).json(errorHandler(409,"user already existe sign in your  account "));
+          }
+          const signUpUser = new USER_MODEL(userBody);
+          signUpUser.save();
+          res.status(201).json({
+               success:true,
+               message:"user created successfully ",
+               statusCode:201,
+          })
+
+     } catch (error) {
+          console.log(error.message)
+          next(error)
+     };
+
+
 }
