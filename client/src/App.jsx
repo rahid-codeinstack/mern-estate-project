3892de5edd5fcc5dadec16a081228c3c760ef387
rahidkhan <rahidkhanbannu1@email.com>
2025-09-06 15:45:21 +0430

    client side create all pages route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import SignOut from "./pages/user pages/signout";
+import SignIn from "./pages/user pages/signin";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import SignUp from "./pages/user pages/Signup";
+import Profile from "./pages/Profile/Profile";
+
+
 
 function App() {
   return (
-    <div className='text-danger text-center text-uppercase  py-3 text-40 '>App</div>
+    <BrowserRouter>
+      <Routes>
+           <Route path="/"        element={<Home/>}/>
+           <Route path="/sign-up" element={<SignUp/> } />
+           <Route path="/sign-in" element={<SignIn/> } />
+           <Route path="/about"   element={<About/>  } />
+           <Route path="/profile" element={<Profile/> } />
+
+      </Routes>
+    </BrowserRouter>
   );
 };
 
