540f20a5fd31169afa4f791ba72064afc6e0a269
rahidkhan <rahidkhanbannu1@email.com>
2025-09-06 15:16:59 +0430

    start server

---
+++ server/Server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+
+// ---------------------------------- request midddlware ---------------------------------------------------- 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+// ------------------------------------- route middlware section ---------------------------------------------
+
+
+
+
+// -------------------------------------- test route ---------------------------------------------------------
+app.get( "/test", ( req  , res ) => {
+     res.send({
+          success:true,
+          message:"  hellow world  ",
+          status:200,
+     });
+} );
+
+
+// -------------------------- app runing --------------------------------------------------------------------------
+
+const portno = process.env.PORTNO || 3000 ;
+app.listen(portno,()=>{
+     console.log( "app running on portno "+ portno );
+})
