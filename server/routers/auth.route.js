8ee613c7b14bc7dd3c413992c9f39f6f7208cd28
rahidkhan <rahidkhanbannu1@email.com>
2025-09-07 17:01:16 +0430

    add limiting to the sign in rounte and sign up rounte

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -1,11 +1,42 @@
 import express from 'express';
 import { signUp , signIn } from '../Controller/auth.controller.js';
-
+import {ipKeyGenerator, rateLimit} from 'express-rate-limit'
 
 const router = express.Router();
+
+const  signInRateLimit = rateLimit({
+          windowMs: 10 * 60 * 1000,
+          max:5,
+          headers:function(req,res,next,options){
+               return res.status(4029).json({
+                         message:' Too many request please try again later ',
+                         success:false,
+                         status:429,
+
+               })
+          },
+          ipKeyGenerator:(req) => req.email || req.ip,
+          skipSuccessfulRequests: true,
+         
+})
+const  signUpRateLimit = rateLimit({
+          windowMs: 10 * 60 * 1000,
+          max:5,
+          headers:function(req,res,next,options){
+               return res.status(429).json({
+                         message:' Too many request please try again later ',
+                         success:false,
+                         status:429,
+
+               })
+          },  
+})
+
+
+
      
-          router.post("/sign-up",signUp);
-          router.post("/sign-in",signIn)
+          router.post("/sign-up", signUpRateLimit, signUp);
+          router.post("/sign-in",signInRateLimit,signIn)
 
 
 export default router;
