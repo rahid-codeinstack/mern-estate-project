348403615299089f4a8fcf3326d05140ca85bc31
rahidkhan <rahidkhanbannu1@email.com>
2025-09-09 10:26:42 +0430

    create updateUser function '

--- server/routers/user.route.js
+++ server/routers/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { verify } from '../utils/verify.js';
+import {updateUser} from '../Controller/user.controller.js';
 
 const router = express.Router();
 
 router.get("/test",verify,(req,res)=>res.json({message:"hellow world"}));
+router.post("/update",verify,updateUser)
 
 
 export default router;
