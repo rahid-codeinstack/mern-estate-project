b1feceadfcc458f09ae8689856684152114d8ae6
rahidkhan <rahidkhanbannu1@email.com>
2025-09-09 09:20:42 +0430

    add uploading funtionality to the profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -7,10 +7,34 @@ function Profile() {
   const [evater,setevater] = useState("");
   const {user} = useSelector(st=>st.user);
   const fileRef = useRef("");
+  const [fileLoading , setfileLoading ] = useState(false);
+  const [isUploaded , setisUploaded ] = useState(false);
   
-  function changeProfile (e){
+  
+  async function changeProfile (e){
     const file = e.target.files[0];
-    console.log(file);
+
+    const fileData =  new FormData();
+    fileData.append("file",file);
+    fileData.append("upload_preset","profile_images");
+  
+    try {
+        setfileLoading(true);
+    const res = await fetch("https://api.cloudinary.com/v1_1/dmooukppx/image/upload",{
+        method:"POST",
+        body:fileData,
+
+    })
+    const data = await res.json();
+    setevater(data.url);
+    setfileLoading(false);
+    setisUploaded(true);
+    setTimeout(() => {
+        setisUploaded(false);
+    }, 2000);
+    } catch (error) {
+        
+    }
 
     
   }
@@ -18,17 +42,21 @@ function Profile() {
 
   return <div className="container-fluid">
     <div className="container m-auto my-4 ">
-        <div className="profile-container bg-white  rounded-3 text-black d-flex flex-column justify-content-star align-items-start border borde-2 border-white m-auto p-2  w-50">
+        <div className="profile-container bg-white  rounded-3 text-black d-flex flex-column justify-content-star align-items-start border borde-2 border-white m-auto p-2 py-5 w-50">
 
 
             <div className="profile-box border border-white m-auto  d-flex justify-content-center align-items-center flex-column">
                 <input hidden type="file" ref={fileRef} onChange={changeProfile} id="file" accept="jpg/*" />
                 <div className="image text-center">
                     {user  && 
-                      <img  onClick={()=>fileRef.current.click()} src={evater  ? evater : user.evater } className="m-auto profile-image" alt="profile" />  
+                      <img  onClick={()=>fileRef.current.click()} src={evater  ? evater : user.evater } className="m-auto profile-image rounded-pill" alt="profile" />  
                     }
                 </div>
-                <span className="text-black">rahidkhan</span>
+                <span className="text-black">
+                    {
+                        fileLoading ? <span style={{color:"var(--slate-color)"}}> uploading....</span>: isUploaded ? <span className="text-success">uploaded successfully</span>:""
+                    }
+                </span>
             </div>
 
             <form className="user-field m-auto mt-3  w-75 d-flex justify-content-center align-items-center flex-column gap-2">
