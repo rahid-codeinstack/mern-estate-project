64adae2e682fb877b0096390d6438d02baf368ef
rahidkhan <rahidkhanbannu1@email.com>
2025-09-10 23:21:39 +0430

    complete create-listing pages

--- client/src/pages/create-listing/CreateListing.jsx
+++ client/src/pages/create-listing/CreateListing.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react'
+import {useNavigate} from 'react-router-dom';
+
 import './CreateListing.css'
 
 function CreateListing() {
      
      const [listingData,setlistingData] = useState({
                name:"",description:"", address:"", type:"rent",parking:false,bath:0,
-               bed:1,furnished:false,offer:false,images:[],regularPrice:50, discountPrice:0,          
+               bed:1,furnished:false,offer:false,images:[],regularPrice:"50", discountPrice:"0",          
      });
     const [FileData, setFileData] = useState({
           FileUploading:false, FileError:"",Files:{},
     })
+    const [ListingError,setListingError] = useState("");
+    const [ListingLoading , setListingLoading] = useState(false);
+    const navigate = useNavigate(null);
 
     useEffect(()=>{
                if(FileData.Files.length+listingData.images.length-1 <= 5){
@@ -71,60 +76,123 @@ function handleDeletFile(deletFile){
      setlistingData((prev)=>({...prev,images:filterFile}));
 
 }
-useEffect(()=>{
-          console.log(listingData.images);
-},[listingData])
+ async function handlePostListing(){
+     listingData.regularPrice=Number(listingData.regularPrice);
+     listingData.discountPrice=Number(listingData.discountPrice);
+     listingData.bed=Number(listingData.bed);
+     listingData.bath=Number(listingData.bath);
+     if(listingData.bed < 1){
+     setListingError('atleast one bed required in listing ');
+     return;
+     }
+     if(listingData.offer && listingData.discountPrice > listingData.regularPrice){
+          setListingError("discount price must less to the regularPrice");
+          return;   
+     }
+     if(listingData.images.length <= 0  ){
+          setListingError("one or two images required for a listing ");
+          return;   
+     }
+     if(listingData.images.length > 5){
+          setListingError("you can add atleast 5 images for create listing ");
+          return;
+     }
+     if(listingData.name.length < 4 ){
+          setListingError("listing name must be atleast 4 character");
+          return;   
+     }
+     if(listingData.name.length > 30){
+          setlistingData("you can name atleast 30 character ");
+          return;
+     }
+     if(listingData.description.length < 10 ){
+          setListingError("description must be atleast 20 character for a document");
+          return;
+     }
+     try {
+          setListingLoading(true);
+          const res = await fetch("/api/user/create-listing",{
+                    method:'POST',
+                    headers:{
+                         'Content-Type':'application/json'
+                    },
+                    body:JSON.stringify(listingData),
+          })
+          const data  = await res.json();
+          if(!data.success){
+               setListingError(data.message);
+               return;
+          }
+          navigate("listing/"+data.listing._id);
+     } catch (error) {
+          setListingError(error.message);
+
+     }
+
+  
+}
   return (
      <div className="container " >
           <div className="container listing-container rounded-2 w-75 mt-4 bg-white p-3 py-6 d-flex gap-4 justify-content-center align-items-center flex-column ">
                <h1 className='text-center text-capitalize text-black fwt-semibold'>Create Listing </h1>
                <div className="row  gutter gy-4 w-100">
                          <div className="col-12 col-lg-7 row-cols-7 w-full  d-flex gap-2 justify-content-center align-items-center flex-column ">
-                              <input type="text" id='name' className='w-100 p-2  rounded-1  border-1 form-control' placeholder='name' />
-                              <input type="text" id='address' className='w-100 p-2 rounded-1  border-1 form-control' placeholder='address' />
-                              <textarea className='w-100 description-box p-2 rounded-1  border-1 form-control resize-none'  id="description" placeholder='description'></textarea>
+                              <input type="text" id='name' onChange={(e)=>setlistingData({...listingData,name:e.target.value})} value={listingData.name} className='w-100 p-2  rounded-1  border-1 form-control' placeholder='name' />
+                              <input type="text" id='address' onChange={(e)=>setlistingData({...listingData,address:e.target.value})} value={listingData.address} className='w-100 p-2 rounded-1  border-1 form-control' placeholder='address' />
+                              <textarea onChange={(e)=>setlistingData({...listingData,description:e.target.value})} value={listingData.description} className='w-100 description-box p-2 rounded-1  border-1 form-control resize-none'  id="description" placeholder='description'></textarea>
                               <div className='d-flex gap-3 justify-content-start flex-wrap align-items-center '>
                                     <div className='d-flex justify-content-center gap-1 align-items-center '>
-                                        <input type="checkbox" id='sale' />
-                                        <label htmlFor="sale" className='text-black'>sale</label>
+                                        <input type="checkbox" onChange={(e)=>setlistingData({...listingData,type:"sale"})} checked={listingData.type === "sale" ? true:false} id='sale' />
+                                        <label htmlFor="sale"  className='text-black'>sale</label>
                                    </div>
                                     <div className='d-flex justify-content-center gap-1 align-items-center '>
-                                        <input type="checkbox" id='rent' />
+                                        <input type="checkbox" onChange={(e)=>setlistingData({...listingData,type:"rent"})} checked={listingData.type==='rent' ? true : false} id='rent' />
                                         <label htmlFor="rent" className='text-black'>rent</label>
                                    </div>
                                     <div className='d-flex justify-content-center gap-1 align-items-center '>
-                                        <input type="checkbox" id='parking' />
+                                        <input type="checkbox" onChange={(e)=>setlistingData({...listingData,parking:e.target.checked ? true : false})} checked={listingData.parking} id='parking' />
                                         <label htmlFor="parking" className='text-black'>parking</label>
                                    </div>
                                     <div className='d-flex justify-content-center gap-1 align-items-center '>
-                                        <input type="checkbox" id='furnished' />
-                                        <label htmlFor="furnished" className='text-black'>furnished</label>
+                                        <input type="checkbox" onChange={(e)=>setlistingData({...listingData,furnished:e.target.checked ? true : false})} checked={listingData.furnished} id='furnished' />
+                                        <label htmlFor="furnished"  className='text-black'>furnished</label>
                                    </div>
                                     <div className='d-flex justify-content-center gap-1 align-items-center '>
-                                        <input type="checkbox" id='offer' />
+                                        <input type="checkbox" onChange={(e)=>setlistingData({...listingData,offer:e.target.checked ? true : false})} checked={listingData.offer} id='offer' />
                                         <label htmlFor="offer" className='text-black'>offer</label>
                                    </div>
 
                               </div>
                               <div className='d-flex w-100 justify-start mt-2 align-items-center gap-3   '>
                                         <div className='d-flex justify-content-start align-items-center gap-2 '>
-                                                  <input type="number" className='form-control w-50  ' id='bed' min={1}/>
+                                                  <input type="number" onChange={(e)=>setlistingData({...listingData,bed:Number(e.target.value)})} value={listingData.bed} className='form-control w-50  ' id='bed' min={1}/>
                                                   <label htmlFor="bed" className='text-black'>bed</label>
                                         </div>
                                         <div className='d-flex justify-content-start align-items-center gap-2 '>
-                                                  <input type="number" className='form-control w-50 ' id='bath' min={1}/>
+                                                  <input type="number" onChange={(e)=>setlistingData({...listingData,bath:Number(e.target.value)})} value={listingData.bath} className='form-control w-50 ' id='bath' min={1}/>
                                                   <label htmlFor="bath" className='text-black'>bath</label>
                                         </div>
                               </div>
                               <div className='w-100 mt-4   '>
                                         <div className=' w-75  gap-3 d-flex  align-items-center '>
-                                               <input type="text" id='regularPrice' className=' form-control m-0' placeholder='regularPrice' />   
-                                               <span className='text-black'>($/month)</span>
+                                               <input type="text"  onChange={(e)=>{setlistingData({...listingData,regularPrice:e.target.value})}} value={listingData.regularPrice}  id='regularPrice' className=' form-control m-0' placeholder='regularPrice' />   
+                                               <span className='text-black'>
+                                                 
+                                                       {
+                                                       listingData.type==='rent' ? "($/month)":"($/sale)"
+                                                        }
+                                               </span>
                                         </div>
-                                        <div className=' w-75 mt-3  gap-3 d-flex  align-items-center '>
-                                               <input type="text" id='descountPrice' className=' form-control m-0' placeholder='descountPrice' />   
-                                               <span className='text-black'>($/month)</span>
-                                        </div>
+                                       {
+                                             listingData.offer &&  <div className=' w-75 mt-3  gap-3 d-flex  align-items-center '>
+                                               <input type="text"  onChange={(e)=>{setlistingData({...listingData,discountPrice:e.target.value})}} value={listingData.discountPrice}  id='disscountPrice' className=' form-control m-0' placeholder='descountPrice' />   
+                                               <span className='text-black'>
+                                                  {
+                                                       listingData.type==='rent' ? "($/month)":"($/sale)"
+                                                  }
+                                               </span>
+                                               </div>
+                                       }
                               </div>
                          </div>
                          <div className="col-12 col-lg-5 w-full  mt-4 mt-md-0 py-4 py-md-0 border border-black">
@@ -150,9 +218,25 @@ useEffect(()=>{
                                    </div>
                          </div>
                </div>
+               <div className='w-75'>
+                    {
+                         ListingError && 
+                         <div className='alert alert-danger w-100 d-flex justify-content-between  align-items-center ' role='alert'>
+                             <span> {ListingError}</span>
+                             <button onClick={()=>setListingError("")} type="button" class=" btn btn-close " aria-label="Close"></button> 
+                         </div>
+                         
+                    }
+               </div>
 
                 <div className='w-100 text-center mt-3'>
-                    <button className='w-75 btn p-2 rounded-2 text-white ' style={{backgroundColor:"var(--slate-color)"}}> CREATE </button>
+                    <button onClick={handlePostListing} className='w-75 btn p-2 rounded-2 text-white ' style={{backgroundColor:"var(--slate-color)"}}> 
+                         {
+                          ListingLoading ?  <div className='spinner-border '>
+                          <span className='visually-hidden'></span>
+                          </div>:"CREATE"
+                         }
+                     </button>
                </div>
           </div>
          
